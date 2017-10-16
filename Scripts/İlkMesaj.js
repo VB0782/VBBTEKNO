ab1a2b683d31987e5fdf2d30cc9ceebb70c6e076
Veli BÜLBÜL <velibulbul12@gmail.com>
2017-10-17 00:12:38 +0300

    Gereksizdi Sildim

--- Scripts/İlkMesaj.js
+++
@@ -1,8 +0,0 @@
-// document.writeln("<font color='yellow'>");
-
-//window.alert("19.07.2007\nFenerbahceSampiyon\nFenerbahceliler Günü Kutlu olsun");
-
-//kodları yazarken harf uyununa lütfen dikkat edelim.
-
-/*var velibulbul ="VBB Yazılıma Hoşgeldiniz!";
-documnet.write("velibulbul");*/
